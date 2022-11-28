a0f7b393947a831bc1c8027511e7df0e5fbe0a1a
hjarai <harai@middlebury.edu>
2022-11-28 14:59:08 -0800

    rgcn conv to its original form

--- cogmen/model/GNN.py
+++ cogmen/model/GNN.py
@@ -1,13 +1,12 @@
 import torch.nn as nn
-from torch_geometric.nn import RGCNConv, TransformerConv, FastRGCNConv
+from torch_geometric.nn import RGCNConv, TransformerConv
 
 
 class GNN(nn.Module):
     def __init__(self, g_dim, h1_dim, h2_dim, args):
         super(GNN, self).__init__()
         self.num_relations = 2 * args.n_speakers ** 2
-        # self.conv1 = RGCNConv(g_dim, h1_dim, self.num_relations)
-        self.conv1 = FastRGCNConv(g_dim, h1_dim, self.num_relations)
+        self.conv1 = RGCNConv(g_dim, h1_dim, self.num_relations)
         self.conv2 = TransformerConv(h1_dim, h2_dim, heads=args.gnn_nheads, concat=True)
         self.bn = nn.BatchNorm1d(h2_dim * args.gnn_nheads)
 
